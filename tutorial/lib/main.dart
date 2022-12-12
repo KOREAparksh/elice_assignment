1d60fe9badaa7e8752f273dbf53ec67e5252b8d2
KOREAparksh <tmdgks2222@naver.com>
2022-12-12 21:41:55 +0900

    Feat: Observer 구현 및 로그 확인

--- tutorial/lib/main.dart
+++ tutorial/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial/observer/count_observer.dart';
 import 'package:tutorial/screen/temp_view.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'cubit/count_cubit.dart';
 
 void main() {
+  Bloc.observer = CountObserver();
   runApp(const MyApp());
 }
 
