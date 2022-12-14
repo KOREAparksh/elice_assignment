0a9d3f9a8b60d871a63c91ec11cfb64461f255bf
KOREAparksh <tmdgks2222@naver.com>
2022-12-15 00:58:21 +0900

    Refactor: course list 분리

--- elice_pa/lib/screen/main/home_screen.dart
+++ elice_pa/lib/screen/main/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:elice_pa/cubit/recommend_course_cubit.dart';
 import 'package:elice_pa/dto/course_dto.dart';
 import 'package:elice_pa/screen/detail_course_screen.dart';
 import 'package:elice_pa/util/converter.dart';
+import 'package:elice_pa/widget/course_list.dart';
 import 'package:elice_pa/widget/course_tile/course_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -182,53 +183,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-}
-
-class CourseListView extends StatelessWidget {
-  const CourseListView({
-    Key? key,
-    required this.courses,
-  }) : super(key: key);
-
-  //Data
-  final List<Course> courses;
-
-  //Size
-  final _listHeight = 200.0;
-  final _cardSeparateWidth = 10.0;
-  final _sidePadding = 16.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: _listHeight,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemCount: (courses.length > 10) ? 10 : courses.length,
-            itemBuilder: _itemBuilder,
-            separatorBuilder: _separatorBuilder,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _itemBuilder(BuildContext context, int index) {
-    double leftPadding = (index == 0) ? _sidePadding : 0;
-    double rightPadding = (index == courses.length - 1) ? _sidePadding : 0;
-
-    return Container(
-      margin: EdgeInsets.only(left: leftPadding, right: rightPadding),
-      child: CourseTile(
-        title: titleConverter(courses[index].title),
-        instructor: instructorConverter(courses[index].instructors),
-        url: courses[index].logoFileUrl,
-      ),
-    );
-  }
-
-  Widget _separatorBuilder(BuildContext context, int index) =>
-      SizedBox(width: _cardSeparateWidth);
 }
